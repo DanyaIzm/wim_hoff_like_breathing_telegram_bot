856d03096462cc5222d055d1345eae0209e135df
Stack Canary <danizmd8@gmail.com>
2024-08-29 17:10:22 +0300

    feat: "add bot count feature"

--- main.py
+++ main.py
@@ -3,12 +3,32 @@ from dataclasses import dataclass
 from os import getenv
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
 from aiogram.types import Message
 from dotenv import load_dotenv
 
 
-def hello(message: Message) -> None:
-    return message.answer(f"Hi! Did you say: {message.text}")
+async def count(message: Message) -> None:
+    seconds_to_wait = 15
+
+    await message.answer("Let the counter start!")
+
+    message = await message.bot.send_message(
+        message.from_user.id, f"Left: {seconds_to_wait}"
+    )
+
+    for current_second in range(0, seconds_to_wait, 5):
+        await asyncio.sleep(5)
+
+        seconds_left = seconds_to_wait - (current_second + 5)
+
+        await message.edit_text(f"Left: {seconds_left}")
+
+    await message.edit_text("Thank you!")
+
+
+async def hello(message: Message) -> None:
+    return await message.answer(f"Hi! Did you say: {message.text}")
 
 
 @dataclass
@@ -27,6 +47,7 @@ async def main() -> None:
 
     dispatcher = Dispatcher()
 
+    dispatcher.message(Command("count"))(count)
     dispatcher.message()(hello)
 
     bot = Bot(token=config.bot_token)
