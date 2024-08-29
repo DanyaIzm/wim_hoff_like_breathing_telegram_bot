be082e212d45532534e2b57fb746e9cbfe1b9437
Stack Canary <danizmd8@gmail.com>
2024-08-29 17:17:12 +0300

    feat(bot): add breathing cycle states

---
+++ states.py
@@ -0,0 +1,12 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class BreathCycle(StatesGroup):
+    rounds_amount_selection = State()
+    seconds_to_hold_amount_selection = State()
+    pre_breathing = State()
+    primary_breath_holding = State()
+    deep_breath = State()
+    secondary_breath_holding = State()
+    round_end = State()
+    session_end = State()
